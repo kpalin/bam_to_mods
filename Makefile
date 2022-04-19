f9d696590797d66c76e34644ca492e45c15f8d60
Kimmo Palin <kimmo.palin@helsinki.fi>
2022-04-19 16:03:45 +0300

    Good progress

--- Makefile
+++ Makefile
@@ -2,11 +2,11 @@ all:bam_to_mods
 
 
 bam_to_mods: obj/bam_mods_to_text.o
-	$(CC) -g -o $@ obj/bam_mods_to_text.o  -lhts $(CFLAGS) $(LDFLAGS)  $(LIBS) -lpthread
+	$(CXX) -g -o $@ obj/bam_mods_to_text.o  -lhts $(CFLAGS) $(LDFLAGS)  $(LIBS) -lpthread
 
 
-obj/bam_mods_to_text.o: src/bam_mods_to_text.c
-	$(CC) -c -g -o $@ src/bam_mods_to_text.c -lhts ${CFLAGS} $(LIBS) -lpthread 
+obj/bam_mods_to_text.o: src/bam_mods_to_text.cpp
+	$(CXX) -c -g -o $@ $^  -lhts ${CFLAGS} $(LIBS) -lpthread 
 
 clean: 
 	rm obj/bam_mods_to_text.o bam_to_mods
---
+++ test/run_clean.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#  Created on Tuesday, 05 April  2022
+
+# For debugging
+#set -o verbose
+
+# Die on unset variables
+set -o nounset
+# Die on errors
+set -o errexit
+# Die if any part of a pipe fails
+set -o pipefail
+LD_PRELOAD=/scratch/kpalin/miniconda3/envs/hts_dev/lib/libmimalloc.so.2.0 MIMALLOC_SHOW_ERRORS=1 MIMALLOC_VERBOSE=1 \
+    ./bam_to_mods /mnt/cg8/reference-genomes/GRCh38_no_alt/GRCh38_no_alt.fasta /mnt/cgnano/projects/promethion/kpalin/dev/Fam_c461_1_19_0711NK_meg/phase/longshot.Fam_c461_1_19_0711NK_meg.phased.cram
